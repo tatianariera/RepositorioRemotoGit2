4a3acd11b5062de3e3d1baf7084a73cd5ab6ab58
horabaixa <horabaixa@daw1-13>
2024-03-05 17:28:07 +0100

    segundo intento

--- Proyecto2/src/EjemplosCtrolVersionesGit/Clase2.java
+++ Proyecto2/src/EjemplosCtrolVersionesGit/Clase2.java
@@ -5,5 +5,7 @@ public class Clase2 {
 	public static void main(String[] args) {
 		System.out.println("No sé qué estoy haciendo.");
 		System.out.println("Socorro");
+		System.out.println("Probando");
+		System.out.println("1 2");
 	}
 }
