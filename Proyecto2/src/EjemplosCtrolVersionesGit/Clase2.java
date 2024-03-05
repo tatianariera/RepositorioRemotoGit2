faf4a2e9d09d04c2c725af447627738c8738b588
horabaixa <horabaixa@daw1-13>
2024-03-05 17:24:55 +0100

    vale

---
+++ Proyecto2/src/EjemplosCtrolVersionesGit/Clase2.java
@@ -0,0 +1,9 @@
+package EjemplosCtrolVersionesGit;
+
+public class Clase2 {
+
+	public static void main(String[] args) {
+		System.out.println("No sé qué estoy haciendo.");
+		System.out.println("Socorro");
+	}
+}
